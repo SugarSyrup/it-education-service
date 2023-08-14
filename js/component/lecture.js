1673a7fb748f99d2708d10ecec5d00ccc6a6660f
SugarSyrup <tlfvm04@naver.com>
2023-08-14 13:24:18 +0900

    sync

---
+++ js/component/lecture.js
@@ -0,0 +1,20 @@
+function Lecture(title, explain, tags, practice) {
+    const div = document.createElement('div');
+    div.className = "lecture";
+    div.innerHTML = `
+        <span class="title">${title}</span>
+        <span class="explain">${explain}</span>
+        <div class="icons">
+            <div class="tags">
+            ${tags.map((tag) => 
+                `<span class="tag">${tag}</span>`
+            )}
+            </div>
+            ${practice && `<span class="practice">실습</span>`}
+        </div>
+    `
+
+    return div;
+}
+
+export default Lecture;
---
+++ js/recommend.js
@@ -0,0 +1,6 @@
+import Lecture from "./component/lecture.js";
+const recommend_wrapper = document.querySelector('.recommend');
+
+recommend_wrapper.append(Lecture("로그인/회원가입","로그인과 회원가입 기능에 대한 강의",["#인터넷"],true))
+
+
