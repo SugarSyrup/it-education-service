b11ef80b5ac9d545768d8d4bf527c5cde5688fbb
SugarSyrup <tlfvm04@naver.com>
2023-08-17 15:05:43 +0900

    main page

--- js/read.js
+++ js/read.js
@@ -2,7 +2,7 @@ import { URL } from "./utils/CONST.js";
 
 const inputs = document.querySelectorAll('input');
 const submit_button = document.querySelector('.button');
-const values = [];
+let values = [];
 
 function getValues() {
     inputs.forEach((input) => {
@@ -12,6 +12,7 @@ function getValues() {
 
 submit_button.addEventListener('click', (e) => {
     getValues();
+    console.log(values);
     fetch(`${URL}/post/getPosts`, {
         method:'POST',
         headers: {
@@ -21,7 +22,16 @@ submit_button.addEventListener('click', (e) => {
             "email" : values[0],
             "password" : values[1]
         })
-    }).then(async (response) => await response.json())
-        .then((response) => {console.log(response)})
-        .catch((error) => {console.log(error)})
+    }).then(async (response) => {
+        return await response.json()
+    })
+        .then((response) => { 
+            localStorage.setItem("post_read",JSON.stringify(response));
+            location.href = "/detail.html";
+        })
+        .catch((error) => {
+            values = [];
+            console.log(error)
+        })
+    values = [];
 })
