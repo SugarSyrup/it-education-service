f6204a6dfcd9d6fc9c9048dea35adc67a4da2892
SugarSyrup <tlfvm04@naver.com>
2023-08-18 23:53:35 +0900

    login/infomation

--- content/login/js/information_page.js
+++ content/login/js/information_page.js
@@ -1,53 +1,69 @@
 const nameInput = document.querySelector("#name");
-let name_check = document.querySelector('#name_check');
+const name_check = document.querySelector("#name_check");
 const emailInput = document.querySelector("#email");
-let email_check = document.querySelector('#email_check');
-const email_list = document.querySelector('#email_list');
-const email_txt = document.querySelector('#email_txt');
-const submit = document.querySelector("#submit");
+const email_check = document.querySelector("#email_check");
+const email_list = document.querySelector("#email_list");
 const year = document.querySelector("#year");
-const month = document.querySelector('#month');
-const day = document.querySelector('#day');
-const divBox = document.querySelector('.email_box');
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const submit_Btn = document.querySelector("#submit");
 const box_email_Btn = document.querySelector('.box_email_button');
 const hint_box = document.querySelector('.hint_box');
-const nextBtn = document.querySelector('.queston');
 let check = 0;
 
-//이름
-function IsEmpty(input, errorLabel, errorMessage){
-  if(input.value == ""){
-    errorLabel.innerText =errorMessage +"입력해주세요.";
-    check = 0;
-  }
-  else if(input.value.includes(" ")){
-    errorLabel.innerText = "공백이 포함되어 있습니다!";
-    errorLabel.style.color = "red";
-    input.style.border = "2px solid red";
-    check = 0;
-  }
-  else
-  {
-    errorLabel.innerText ="";
-    errorLabel.style.color = "white";
-    input.style.border = "1px solid #ccc";
-    check = 1;
-  }
-}
-//이메일
+const name_x = /^[ㄱ-ㅎ]|[가-힣]{1,4}$/;
+const name_y = /^[a-z|A-Z|0-9]+$/;
 
-email_list.addEventListener('change', (event) => {
-  if(event.target.value !== "type") {
-    email_txt.value = event.target.value
-    email_txt.disabled = true
-  } else {
-    email_txt.value = ""
-    email_txt.disabled = false
+nameInput.addEventListener("input", function(){
+  if(nameInput.value.includes(" ")){
+    name_check.style.color = "red";
+    name_check.innerText = "공백이 포함되어 있습니다";
+    check = 0;
+  }
+  else if(name_x.test(nameInput.value) == false){
+    name_check.style.color = "red";
+    name_check.innerText = "한글로 입력해주세요";
+    check = 0;
+  }
+  else{
+    name_check.style.color = "black";
+    name_check.innerText = "";
+    check = 1;
   }
 })
 
+emailInput.addEventListener("input", function(){
+  if(emailInput.value.includes(" ")){
+    email_check.style.color = "red";
+    email_check.innerText = "공백이 포함되어 있습니다";
+    check = 0;
+  }
+  else if(name_y.test(emailInput.value) == false){
+    email_check.style.color = "red";
+    email_check.innerText = "영어로 입력해주세요";
+    check = 0;
+  }
+  else{
+    email_check.style.color = "black";
+    email_check.innerText = "";
+    check = 1;
+  }
+})
 
-function submit_btn(event){
+email_list.addEventListener("click", function(){
+  if(email_list.value == "type"){
+    email_check.style.color = "red";
+    email_check.innerText = "메일 주소를 입력해주세요";
+    check = 0;
+  }
+  else{
+    email_check.style.color = "black";
+    email_check.innerText = "";
+    check = 1;
+  }
+})
+
+submit_Btn.addEventListener("click", function(event){
   event.preventDefault();
   if(check == 0){
     Swal.fire({
@@ -57,26 +73,25 @@ function submit_btn(event){
     })
   }
   else{
-    Swal.fire({
-      title: '성공!',
-      icon: 'success'
-    })
-    localStorage.setItem('name', nameInput.value);
-    localStorage.setItem('ymd', year.value + month.value + day.value);
-      if(email_txt.disabled = false){
-      localStorage.setItem('email',emailInput.value+'@'+email_list.value)
-      }
-      else{
-      localStorage.setItem('email',emailInput.value+'@'+email_txt.value)
-      }
+    if(year.value == "" || day.value == "" || month.value == ""){
+      Swal.fire({
+        title: '실패!',
+        icon: 'error',
+        text: '생년월일을 입력해주세요!',
+      })
     }
-}
+    else{
+      Swal.fire({
+        title: '성공!',
+        icon: 'success',
+      })
+      localStorage.setItem('name', nameInput.value);
+      localStorage.setItem('ymd', year.value + month.value + day.value);
+      localStorage.setItem('email', emailInput.value +'@'+email_list.value)
+    }
+  }
+})
 
-nameInput.addEventListener('input',() => IsEmpty(nameInput, name_check, "성함"));
-emailInput.addEventListener('input',() => IsEmpty(emailInput, email_check, "이메일"));
-// email_txt.addEventListener('input',() => IsEmpty(email_txt, email_check, "이메일 주소") )
-
-submit.addEventListener('click', submit_btn);
 box_email_Btn.addEventListener('click', (event) => {
   event.preventDefault();
   if(hint_box.classList[1] == "remove"){
